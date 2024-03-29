8e0a794fe85413149a62afbf0e6512676b131f7d
gkju <gkjuszczyk@gmail.com>
2024-03-29 14:31:30 +0100

    init repo

---
+++ gauss-binom/src/MathUtils.java
@@ -0,0 +1,13 @@
+import org.apache.commons.math3.complex.Complex;
+
+import java.util.ArrayList;
+
+public class MathUtils {
+    public static void fft(ArrayList<Complex> vec, boolean invert) {
+        if(vec.size() == 1) {
+            return;
+        }
+
+        var nthRoot = nthRoot.
+    }
+}
